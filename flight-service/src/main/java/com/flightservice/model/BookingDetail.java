38edc22615763d04c794579cf117a6ab4ab21551
shubhamkumar11 <shubham.kumar11@nagarro.com>
2023-04-13 11:23:46 +0530

    adding flight service

---
+++ flight-service/src/main/java/com/flightservice/model/BookingDetail.java
@@ -0,0 +1,97 @@
+package com.flightservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class BookingDetail {
+
+    private String username;
+
+    private String name;
+
+    private Long flightId;
+
+    private Long busId;
+
+    private Long hotelId;
+
+    private Long seatNo;
+
+    private Long rooomNo;
+
+    public BookingDetail(){
+
+    }
+
+    public BookingDetail( String username, String name, Long flightId, Long busId, Long hotelId, Long seatNo, Long rooomNo) {
+
+        this.username = username;
+        this.name = name;
+        this.flightId = flightId;
+        this.busId = busId;
+        this.hotelId = hotelId;
+        this.seatNo = seatNo;
+        this.rooomNo = rooomNo;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public Long getBusId() {
+        return busId;
+    }
+
+    public void setBusId(Long busId) {
+        this.busId = busId;
+    }
+
+    public Long getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Long hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Long getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(Long seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public Long getRooomNo() {
+        return rooomNo;
+    }
+
+    public void setRooomNo(Long rooomNo) {
+        this.rooomNo = rooomNo;
+    }
+}
